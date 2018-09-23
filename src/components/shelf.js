6408cc93aa86748a67433f58efcc987b73d55b8a
Sara Krum <sarasmith887@gmail.com>
2018-09-23 16:41:00 -0400

    book and shelf components working

--- src/components/shelf.js
+++ src/components/shelf.js
@@ -1,43 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Book from '../components/book'
+
 
 class Shelf extends React.Component {
 	render () {
 		return (
-			<div className="list-books">
-
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      
+                    <Book /> 
                     </ol>
                   </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      
-                    </ol>
-                  </div>
-                </div>
-            </div>
-            </div>
             </div>
             )
 	}
